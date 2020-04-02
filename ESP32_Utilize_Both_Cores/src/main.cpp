c4e8b43030de0b4cabcd77f3df5b2ce683756c72
Zissis Papadopoulos <11696874+zissis-pap@users.noreply.github.com>
2020-04-02 19:10:01 +0300

    Update main.cpp

--- ESP32_Utilize_Both_Cores/src/main.cpp
+++ ESP32_Utilize_Both_Cores/src/main.cpp
@@ -5,16 +5,15 @@ TaskHandle_t OnCore0;
 // Function to run on core 0
 void Core0(void *pvParameters);
 
-
 void setup() {
   // Create a task that will be executed on core 0 in the Core0() function, with priority 1.
   xTaskCreatePinnedToCore(
                     Core0,   /* Task function. */
-                    "OnCore0",     /* name of task. */
+                    "OnCore0",   /* name of task. */
                     10000,       /* Stack size of task */
                     NULL,        /* parameter of the task */
                     1,           /* priority of the task */
-                    &OnCore0,      /* Task handle to keep track of created task */
+                    &OnCore0,    /* Task handle to keep track of created task */
                     0);          /* pin task to core 0 */                  
 
 }
